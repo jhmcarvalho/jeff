7172b619efcee712817f9d29232ebd8299da1926
jhmcarvalho <jeferson_funet@hotmail.com>
2022-07-13 10:53:26 -0300

    Ãdicionado hashmap para ler os dados de um arquivo em vez de adicionar as frases direto no código.

--- Frases/src/frases/ViewFrases.java
+++ Frases/src/frases/ViewFrases.java
@@ -46,7 +46,7 @@ public class ViewFrases {
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	public void initialize() {
 
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.WHITE);
@@ -69,10 +69,10 @@ public class ViewFrases {
 			public void actionPerformed(ActionEvent arg0) {
 
 			//	if (txtFrases.getText() != null) {
-				//		JOptionPane.showMessageDialog(null, lista);
+						//JOptionPane.showMessageDialog(null, +result);
 				//	} else {
 				//		JOptionPane.showMessageDialog(null,
-				//					"Sua frase não pode ser gerada");
+				//					"Sua frase nÃ£o pode ser gerada");
 				//		}
 
 			}
@@ -99,61 +99,7 @@ public class ViewFrases {
 
 	// public boolean checkFrases(String frases){
 	// return frases.equals("frases");
-	// }
+ }
 	
 	
-	//CLASSE FRASES
-	public class Frases {
-		public String sorteia() {
-			List<String> lista = new ArrayList<String>();
-			lista.add("Sempre lutei por liberdade, mas ser livre me fez só."); // 1
-			lista.add("Não corra atrás de alguém que não dá um passo por você."); // 2
-			lista.add("A dor mora na lembrança dos momentos felizes."); // 3
-			lista.add("Não desista fácil, nem insista pra sempre."); // 4
-			lista.add("Se você se tornar uma opção, não queira ser escolhido. Seja prioridade."); // 5
-			lista.add("Ninguém reconhece o que você faz, até você deixar de fazer."); // 6
-			lista.add("Foi bom enquanto durou, e durou enquanto estava sendo bom."); // 7
-			lista.add("Seja você mesmo, as pessoas certas irão aparecer."); // 8
-			lista.add("A maldade não deve ser devolvida. Deixe a vida cobrar."); // 9
-			lista.add("Não se apague a algo que te faz mal, só porque as vezes te faz bem."); // 10
-			lista.add("Não chame o outro de emocionado se é você quem não sabe receber afeto."); // 11
-			lista.add("Tem gente que não quer te perder, mas não sabe te amar."); // 12
-			lista.add("A gente não precisa deixar de ser bom, só precisa deixar de ser besta"); // 13
-			lista.add("Eu te quis pelo que você era, e te deixei ir pelo que você começou a ser."); // 14
-			lista.add("Viver não é esperar a tempestade passar, é aprender a dançar na chuva."); // 15
-			lista.add("Queira o bem. Faça o bem. O resto vem."); // 16
-			lista.add("Dê valor a quem te dá amor."); // 17
-			lista.add("Deixe que sua fé seja maior que seus problemas."); // 18
-			lista.add("Se é para ter crise, que seja de riso."); // 19
-			lista.add("Faz bem ser o bem de alguém."); // 20
-			lista.add("Aprendi o que era certo, com a pessoa errada."); // 21
-			lista.add("Um estômago com fome, uma carteira vazia e um coração partido te ensinam as melhores lições da vida."); // 22
-			lista.add("Continue, sempre haverá um bom motivo para ser feliz de novo."); // 23
-			lista.add("De virgula em virgula, uma hora chega o ponto final."); // 24
-			lista.add("Existem amores que só podem viver em seu coração, não em sua vida."); // 25
-			lista.add("Quando os olhos chamarem mais atenção do que qualquer outra parte do corpo, novos sentimentos irão invadir seu coração."); // 26
-			lista.add("Uma mensagem chegou... Dizia apenas: 'Saudade'. Sorri e apaguei... Tem coisas na vida que a gente só erra uma vez."); // 27
-			lista.add("Cuide bem daquilo que você ama. É caro demais perder o que não tem preço."); // 28
-			lista.add("Tem gente que escolhe te perder ao invés de buscar melhorar. Você não pode nudar alguém, mas pode mudar de lugar e seguir.");// 29
-			lista.add("Antes de falar que eu te abandonei, pergunta se tu fazia questão de me ter por perto.");// 30
-			lista.add("Põe-te no lugar do outro, e te tornarás incapaz de julgá-lo.");// 31
-			lista.add("A medida do amor é amar sem medidas.");// 32
-			lista.add("Não importa a cor do céu, quem faz o dia bonito é você.");// 33
-			lista.add("Não tenha medo de descobrir que você não é mais o mesmo");// 34
-			lista.add("Faça alguém feliz hoje. Pequenos gestos fazem toda a diferença.");// 35
-			lista.add("Não deixe as incertezas atrapalharem sua felicidade.");// 36
-
-			Collections.shuffle(lista); // 14
-			// pega qualquer indice.
-			return lista.get((int) Math.random() * lista.size());
-
-		}
-
-		public void main(String[] args) {
-			Frases sorteia = new Frases();
-			for (int i = 0; i < 1; i++) {
-				System.out.println(sorteia.sorteia());
-			}
-		}
-	}
-}
+	
